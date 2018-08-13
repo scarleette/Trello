a77a2d60dd363eada405bc7b6f8b7cf81a70c552
Scarlett <scar.antoine@gmail.com>
2018-08-13 16:17:53 -0300

    Estilo para la tarjeta que se despliega

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,17 @@ function agregarTarea() {
 };
 function agregarTarjeta() {
   const tarjetaNueva = document.getElementById('tarjetaNueva');
-  tarjetaNueva.innerHTML = '<div class="container"><div class="row"><div class="card" style="width: 16rem;"><input type="text" placeholder ="nombre de la tarjeta"><div id = "tareaNueva"></div><input type="button" value="Agregar Tarea" onclick="agregarTarea()"></div></div></div>';
+  tarjetaNueva.innerHTML += 
+  `<div class="card" style="width: 16rem;">
+      <div class="card-body">
+      <input type="text" placeholder ="nombre de la tarjeta">
+      <div id = "tareaNueva"></div> 
+    <input type="button" value="Agregar Tarea" onclick="agregarTarea()">  
+  </div>
+</div>`;
+  
 };
 
 function inspeccionarTarea() {
   
 }
-
