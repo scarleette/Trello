c643d325bca5d70a49a2c4dacc7a2bb45afe2c55
Scarlett <scar.antoine@gmail.com>
2018-08-14 12:45:44 -0300

    trabajando con dom

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,28 @@
-function agregarTarea() {
-  const tareaNueva = document.getElementById('tareaNueva');
-  tareaNueva.innerHTML += '<input type="button" value="tarea 1" onclick="inspeccionarTarea()">';
-};
 function agregarTarjeta() {
-  const tarjetaNueva = document.getElementById('tarjetaNueva');
-  tarjetaNueva.innerHTML += 
-  `<div class="card" style="width: 16rem;">
-      <div class="card-body">
-      <input type="text" placeholder ="nombre de la tarjeta">
-      <div id = "tareaNueva"></div> 
-    <input type="button" value="Agregar Tarea" onclick="agregarTarea()">  
-  </div>
-</div>`;
-  
+  const tareaNueva = document.getElementById('tareaNueva');
+  tareaNueva.innerHTML += `
+  <input class="btn btn-secondary botones type="button" value="tarea 1" onclick="inspeccionarTarea()">`;
+};
+function agregarLista() {
+  // div que esta en html
+  const bton = document.getElementById('boton');
+  bton.style.display = 'none';
+  const listaNueva = document.getElementById('listaNueva');
+  const lista = document.createElement('div');
+  const tituloTarjeta = document.createElement('input');
+  const divParaTarjeta = document.createElement('div');
+  const btn = document.createElement('button');
+  const textoBtn = document.createTextNode('+ Agregar lista');
+
+  divParaTarjeta.setAttribute('id', 'tareaNueva');
+  lista.setAttribute('id', 'lista');
+  btn.onclick = function() {agregarTarjeta()};
+ 
+  listaNueva.appendChild(lista);
+  lista.appendChild(tituloTarjeta);
+  lista.appendChild(divParaTarjeta);
+  lista.appendChild(btn);
+  btn.appendChild(textoBtn);
 };
 
 function inspeccionarTarea() {
